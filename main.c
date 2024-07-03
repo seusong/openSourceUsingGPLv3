4ccb5953711f46e29b044cbd74d1d5b753b7a56d
Seu07 <seusong0007@gmail.com>
2024-07-03 23:29:25 +0700

    complete GNU

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ * This file is part of MyOpenSourceProject.
+ *
+ * MyOpenSourceProject is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyOpenSourceProject is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyOpenSourceProject. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+// #include <stdlib.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    // printf("Calling Wget...\n");
+
+    // Call the Wget executable installed by Homebrew
+    // system("wget --version");
+
+    return 0;
+}
